ea7c8362113600703463a2c4146727dbe5230e7a
hellosurya1010 <ayrus4444@gmail.com>
2023-02-01 18:07:00 +0530

    style and script path changed

--- resources/views/layouts/partials/_headerLinks.blade.php
+++ resources/views/layouts/partials/_headerLinks.blade.php
@@ -3,32 +3,32 @@
 <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
 <meta content="Themesbrand" name="author" />
 <!-- App favicon -->
-<link rel="shortcut icon" href="/images/favicon.png">
+<link rel="shortcut icon" href="{{ asset('/velzon/assets/images/favicon.ico') }}">
 
 <!-- jsvectormap css -->
-<link href="/velzon/assets/libs/jsvectormap/css/jsvectormap.min.css" rel="stylesheet" type="text/css" />
+<link href="{{ asset('/velzon/assets/libs/jsvectormap/css/jsvectormap.min.css') }}" rel="stylesheet" type="text/css" />
 
 <!-- gridjs css -->
-<link rel="stylesheet" href="/velzon/assets/libs/gridjs/theme/mermaid.min.css">
+<link rel="stylesheet" href="{{ asset('/velzon/assets/libs/gridjs/theme/mermaid.min.css') }}">
 
 {{-- jquery --}}
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="{{ asset('https://code.jquery.com/jquery-3.6.0.min.js') }}" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
 <!-- Layout config Js -->
-<script src="/velzon/assets/js/layout.js"></script>
+<script src="{{ asset('/velzon/assets/js/layout.js') }}"></script>
 <!-- Bootstrap Css -->
-<link href="/velzon/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<link href="{{ asset('/velzon/assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
 <!-- Icons Css -->
-<link href="/velzon/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+<link href="{{ asset('/velzon/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
 <!-- App Css-->
-<link href="/velzon/assets/css/app.min.css" rel="stylesheet" type="text/css" />
-<link href="/css/ui.init.css" rel="stylesheet" type="text/css" />
-<link href="/css/star-one.css" rel="stylesheet" type="text/css" />
+<link href="{{ asset('/velzon/assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('/css/ui.init.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('/css/star-one.css') }}" rel="stylesheet" type="text/css" />
 <!-- Tostify Css-->
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
-<!-- custom Css-->
-<link href="/velzon/assets/css/custom.min.css" rel="stylesheet" type="text/css" />
- <!--select2 cdn-->
+<!--select2 cdn-->
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<!-- custom Css-->
+<link href="{{ asset('/velzon/assets/css/custom.min.css') }}" rel="stylesheet" type="text/css" />
   <!-- Sweet Alert css-->
-  <link href="/velzon/assets/libs/sweetalert2/sweetalert2.min.css" rel="stylesheet" type="text/css" />
+  <link href="{{ asset('/velzon/assets/libs/sweetalert2/sweetalert2.min.css') }}" rel="stylesheet" type="text/css" />
