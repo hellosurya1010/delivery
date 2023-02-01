1d92676a6fda54a1d976ebce79846e6201c6f19f
hellosurya1010 <ayrus4444@gmail.com>
2023-02-01 18:57:34 +0530

    asset function retrived

--- resources/views/layouts/partials/_footerLinks.blade.php
+++ resources/views/layouts/partials/_footerLinks.blade.php
@@ -1,30 +1,30 @@
 <!-- JAVASCRIPT -->
-<script src="{{ assetPath('/velzon/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ assetPath('/velzon/assets/libs/simplebar/simplebar.min.js') }}"></script>
-<script src="{{ assetPath('/velzon/assets/libs/node-waves/waves.min.js') }}"></script>
-<script src="{{ assetPath('/velzon/assets/libs/feather-icons/feather.min.js') }}"></script>
-<script src="{{ assetPath('/velzon/assets/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
-<script src="{{ assetPath('/velzon/assets/js/plugins.js') }}"></script>
+<script src="{{ asset('/velzon/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('/velzon/assets/libs/simplebar/simplebar.min.js') }}"></script>
+<script src="{{ asset('/velzon/assets/libs/node-waves/waves.min.js') }}"></script>
+<script src="{{ asset('/velzon/assets/libs/feather-icons/feather.min.js') }}"></script>
+<script src="{{ asset('/velzon/assets/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
+<script src="{{ asset('/velzon/assets/js/plugins.js') }}"></script>
 
 <!-- apexcharts -->
-<script src="{{ assetPath('/velzon/assets/libs/apexcharts/apexcharts.min.js') }}"></script>
+<script src="{{ asset('/velzon/assets/libs/apexcharts/apexcharts.min.js') }}"></script>
 
 <!-- Vector map-->
-<script src="{{ assetPath('/velzon/assets/libs/jsvectormap/js/jsvectormap.min.js') }}"></script>
-<script src="{{ assetPath('/velzon/assets/libs/jsvectormap/maps/world-merc.js') }}"></script>
+<script src="{{ asset('/velzon/assets/libs/jsvectormap/js/jsvectormap.min.js') }}"></script>
+<script src="{{ asset('/velzon/assets/libs/jsvectormap/maps/world-merc.js') }}"></script>
 
 <!-- gridjs js -->
-<script src="{{ assetPath('/velzon/assets/libs/gridjs/gridjs.umd.js') }}"></script>
+<script src="{{ asset('/velzon/assets/libs/gridjs/gridjs.umd.js') }}"></script>
 
 <!--select2 cdn-->
-<script src="{{ assetPath('https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js') }}"></script>
+<script src="{{ asset('https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js') }}"></script>
 
 <!-- App js -->
-{{-- <script src="{{ assetPath('/velzon/assets/js/app.js') }}"></script> --}}
-<script src="{{ assetPath('/js/ui.init.js') }}"></script>
-<script src="{{ assetPath('/js/star-one.js') }}"></script>
+{{-- <script src="{{ asset('/velzon/assets/js/app.js') }}"></script> --}}
+<script src="{{ asset('/js/ui.init.js') }}"></script>
+<script src="{{ asset('/js/star-one.js') }}"></script>
 
 <!-- Sweet Alerts js -->
-<script src="{{ assetPath('/velzon/assets/libs/sweetalert2/sweetalert2.min.js') }}"></script>
+<script src="{{ asset('/velzon/assets/libs/sweetalert2/sweetalert2.min.js') }}"></script>
 <!-- Tostify Css-->
-<script type="text/javascript" src="{{ assetPath('https://cdn.jsdelivr.net/npm/toastify-js"></script>
+<script type="text/javascript" src="{{ asset('https://cdn.jsdelivr.net/npm/toastify-js"></script>
